0c662c85d9622ec79092cd712a867f0d186039aa
Devlin Duldulao <webmasterdevlin@gmail.com>
2019-02-24 01:30:05 -0500

    create cardTracker

--- src/Card/Cards.js
+++ src/Card/Cards.js
@@ -1,40 +1,26 @@
 import React, { Component } from "react";
-import { StyleSheet, Image, NetInfo, ScrollView } from "react-native";
+import { Image, ScrollView, StyleSheet } from "react-native";
 import {
-  Container,
-  Header,
-  Footer,
-  Content,
-  View,
-  Text,
-  H1,
-  H2,
-  H3,
-  Badge,
-  List,
-  Form,
-  Input,
-  Item,
-  ListItem,
   Button,
-  Icon,
-  Toast,
-  Thumbnail,
-  Title,
   Card,
   CardItem,
+  Container,
   DeckSwiper,
-  Body,
-  Left,
-  Right,
+  Footer,
   FooterTab,
-  SwipeRow
+  H1,
+  H2,
+  Icon,
+  Text,
+  View
 } from "native-base";
 import { getDrawACard, getShuffleTheCards } from "./CardService";
 
 class Cards extends Component {
   state = {
+    cardTracker: 0,
     tapped: false,
+    rule: "",
     deck: {
       remaining: null,
       success: false,
@@ -66,8 +52,18 @@ class Cards extends Component {
     // this.setState({ tapped: true });
   };
 
+  handleOnSwipe = () => {
+    // const currentDeck = { ...this.state.deck };
+    // currentDeck.cards = this.state.deck.cards.slice(1);
+    // this.setState({ deck: currentDeck });
+    const count = this.state.cardTracker + 1;
+    this.setState({ cardTracker: count });
+
+    console.warn(this.state.deck.cards[count].code);
+  };
+
   render() {
-    const { tapped } = this.state;
+    const { tapped, deck } = this.state;
     return (
       <Container>
         {tapped ? (
@@ -75,7 +71,9 @@ class Cards extends Component {
             style={{ marginTop: 30, flex: 1, justifyContent: "space-between" }}
           >
             <DeckSwiper
-              dataSource={this.state.deck.cards}
+              onSwipeLeft={() => this.handleOnSwipe()}
+              onSwipeRight={() => this.handleOnSwipe()}
+              dataSource={deck.cards}
               style={styles.container}
               renderEmpty={() => (
                 <View style={{ alignSelf: "center" }}>
@@ -100,32 +98,40 @@ class Cards extends Component {
           </View>
         ) : (
           <View style={styles.container}>
-            <H1 onPress={() => this.setState({ tapped: true })}>
+            <H1
+              style={{ color: "#F44336" }}
+              onPress={() => this.setState({ tapped: true })}
+            >
               Tap to draw the 1st card!
             </H1>
           </View>
         )}
         <View style={{ marginLeft: 20, marginRight: 20, flex: 0.25 }}>
-          <ScrollView>
-            <H2>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
-              blanditiis consectetur dolores doloribus et fugiat, illum ipsa
-              ipsam iure iusto molestiae molestias natus neque numquam odio
-              officiis provident quam sed. Lorem ipsum dolor sit amet,
-              consectetur adipisicing elit. Aut blanditiis consectetur dolores
-              doloribus et fugiat, illum ipsa ipsam iure iusto molestiae
-              molestias natus neque numquam odio officiis provident quam sed.
-            </H2>
-          </ScrollView>
+          {tapped && (
+            <ScrollView>
+              <H2>{this.state.deck.cards[this.state.cardTracker].code}</H2>
+            </ScrollView>
+          )}
         </View>
         <Footer style={styles.footer}>
           <FooterTab>
             <Button vertical>
-              <Icon name="home" />
+              <Icon style={{ color: "#FF4081" }} name="home" />
               <Text>Home</Text>
             </Button>
+            <Button
+              vertical
+              onPress={() => {
+                console.warn(
+                  this.state.deck.cards[this.state.cardTracker].code
+                );
+              }}
+            >
+              <Icon style={{ color: "#FF4081" }} name="paper" />
+              <Text>Rules</Text>
+            </Button>
             <Button vertical>
-              <Icon name="shuffle" />
+              <Icon style={{ color: "#FF4081" }} name="shuffle" />
               <Text>Reshuffle</Text>
             </Button>
           </FooterTab>
@@ -146,7 +152,6 @@ const styles = StyleSheet.create({
     paddingTop: 40
   },
   footer: {
-    backgroundColor: "#fff",
     alignItems: "center"
   },
   footerText: {
